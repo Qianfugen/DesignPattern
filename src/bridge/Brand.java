61cf3623d5822597211dfb15214416aeabb3d7e2
qianfg <15708915671@163.com>
2020-06-15 19:18:11 +0800

    桥接模式

---
+++ src/bridge/Brand.java
@@ -0,0 +1,8 @@
+package bridge;
+
+//手机功能接口
+public interface Brand {
+    void open();
+    void close();
+    void call();
+}
---
+++ src/bridge/Client.java
@@ -0,0 +1,36 @@
+package bridge;
+
+public class Client {
+    public static void main(String[] args) {
+        //获取折叠式手机(样式+品牌)
+        Phone phone1 = new FoldedPhone(new XiaoMi());
+        phone1.open();
+        phone1.close();
+        phone1.call();
+
+        System.out.println("***************************************");
+
+        //获取折叠式Vivo手机
+        Phone phone2 = new FoldedPhone(new Vivo());
+        phone2.open();
+        phone2.close();
+        phone2.call();
+
+        System.out.println("***************************************");
+
+        //获取直立式手机(样式+品牌)
+        Phone phone3 = new UpRightPhone(new XiaoMi());
+        phone3.open();
+        phone3.close();
+        phone3.call();
+
+        System.out.println("***************************************");
+
+        //获取直立式Vivo手机
+        Phone phone4 = new UpRightPhone(new Vivo());
+        phone4.open();
+        phone4.close();
+        phone4.call();
+
+    }
+}
